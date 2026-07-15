dfbb05865d34001093aee0b72f620adb19a25cd7
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-15 19:57:36 +0530

    problem 1 - solution 1 reuseable

--- problem1/problem1.py
+++ problem1/problem1.py
@@ -1,13 +1,14 @@
-# import array as arr
 from array import *
 num = array('i',[2, 3, 4, 5, 6])
 
-count_even = 0
-count_odd = 0
-for i in num:
-    if i%2==0:
-        count_even += 1
-    else:
-        count_odd += 1
+def array_fun(num):
+    count_even = 0
+    count_odd = 0
+    for i in num:
+        if i%2==0:
+            count_even += 1
+        else:
+            count_odd += 1
+    return (f"Even number count in array is :{count_even}\nOdd number count in array is: {count_odd}")
 
-print(f"Even number count in array is :{count_even}\nOdd number count in array is: {count_odd}")
+print(array_fun(num))
