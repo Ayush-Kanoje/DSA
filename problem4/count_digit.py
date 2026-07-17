609692585be6785caa761ad57c7917dc3fcb4b9e
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-07-18 00:13:50 +0530

    added problem 4 txt

--- problem4/count_digit.py
+++ problem4/count_digit.py
@@ -1,14 +1,17 @@
 n = 546213
 num = n
-# count = 0
+count = 0
 
-# while num>0:
-#     count +=1 
-#     num = num // 10
+while num>0:
+    count +=1 
+    num = num // 10
 
-# print(f"total count of no present inside a int variable: {count}")
+print(f"total count of no present inside a int variable: {count}")
 
 
+
+
+#another method using str
 def digitcount(num):
     return len(str(num))
 
