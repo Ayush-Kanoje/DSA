c4f2e3eb96711ec5cb27219a1cfe664899f6e8d1
Ayush Kanoje <ayushkanoje056@gmail.com>
2026-09-04 15:44:52 +0530

    solved GFG DSA problem

---
+++ Easy Level/GFG/solution.py
@@ -0,0 +1,13 @@
+from math import sqrt
+         
+class Solution:
+    def countFactors (self, num):
+        count = 0
+        for val in range(1, int(sqrt(num))+1):
+            if num%val == 0:
+                count +=1
+
+                if num//val != val:
+                    count += 1
+                    
+        return num_list     
